ef15eb82ccc690a0099d4aa0e27a30f734604fda
Julio César G. Costa <51680369+JCGCosta@users.noreply.github.com>
2023-11-19 10:50:14 +0000

    fix/serialize-commchannels-commresources
    fix/serialize-commchannels-commresources

--- app/dto/comm_channels_and_comm_resources_dto.py
+++ app/dto/comm_channels_and_comm_resources_dto.py
@@ -13,13 +13,25 @@ class CommChannelsAndCommResourcesDto(graphene.ObjectType):
 def serialize_comm_channels_and_comm_resources(results):
     comm_channels_and_comm_resources_dto_array = []
     for result in results:
-        obj = {
-            'channel': result['cc']['type'],
-            'resource': result['a']['country_id'] + result['a']['number']
-                                    if result['a'].get('country_id') is not None else result['a']['email'],
-            'weight': result['PROPERTIES(h)']['preference_weight'],
-        }
-        print(obj)
+        obj = None
+        if result['a'].get('country_id') is not None:
+            obj = {
+                'channel': result['cc']['type'],
+                'resource': result['a']['country_id'] + result['a']['number'],
+                'weight': result['PROPERTIES(h)']['preference_weight'],
+            }
+        if result['a'].get('email') is not None:
+            obj = {
+                'channel': result['cc']['type'],
+                'resource': result['a']['email'],
+                'weight': result['PROPERTIES(h)']['preference_weight'],
+            }
+        if result['a'].get('app_name') is not None:
+            obj = {
+                'channel': result['cc']['type'],
+                'resource': result['a']['app_name'],
+                'weight': result['PROPERTIES(h)']['preference_weight'],
+            }
         comm_channels_and_comm_resources_dto_array.append(obj)
 
     return comm_channels_and_comm_resources_dto_array
