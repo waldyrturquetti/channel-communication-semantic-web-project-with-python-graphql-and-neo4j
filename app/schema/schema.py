ccef0850d54fe74bb6e2fa06f642bb62d9e47d99
Julio César G. Costa <51680369+JCGCosta@users.noreply.github.com>
2023-11-19 15:34:12 +0000

    feature/add_strat_queries
    add comm_resouces by comm_channel
    add actions by comm_channel
    add user_contacts relationships by user

--- app/schema/schema.py
+++ app/schema/schema.py
@@ -5,9 +5,18 @@ from app.schema.mutation.create_user import CreateUser
 from app.schema.query.get_user import GetUser
 from app.schema.query.get_user_commchannels import GetUserCommChannels
 from app.schema.query.get_comm_channels_and_comm_resources_by_user import GetCommChannelsAndCommResourcesByUser
+from app.schema.query.get_usercontact_relationships import GetUserContactsRelationship
+from app.schema.query.get_actions_by_comm_channel import GetActionByCommChannel
+from app.schema.query.get_comm_resources_by_comm_channel import GetCommResourcesByCommChannel
 
 
-class Query(GetUser, GetUserCommChannels, GetCommChannelsAndCommResourcesByUser, graphene.ObjectType):
+class Query(GetUser,
+            GetUserCommChannels,
+            GetCommChannelsAndCommResourcesByUser,
+            GetUserContactsRelationship,
+            GetActionByCommChannel,
+            GetCommResourcesByCommChannel,
+            graphene.ObjectType):
     pass
 
 
